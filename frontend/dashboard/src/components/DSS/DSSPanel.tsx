d3502554bd3de962d1e6933acb6aaf44927a2fe3
Priyank Adhav <priyankadhav03@gmail.com>
2025-09-23 00:12:20 +0530

    fix(dss): remove redundant imports

--- frontend/dashboard/src/components/DSS/DSSPanel.tsx
+++ frontend/dashboard/src/components/DSS/DSSPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { VillageSelector } from './VillageSelector';
 import { RecommendationsList } from './RecommendationsList';
 import { DSSStats } from './DSSStats';
