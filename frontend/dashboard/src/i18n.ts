1f9ec13d47a92f05cef15fbb179d1c9b49641f67
Priyank Adhav <priyankadhav03@gmail.com>
2025-10-02 18:20:43 +0530

    create i18n file to enable language translation

---
+++ frontend/dashboard/src/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./locales/en/translation.json";
+import hi from "./locales/hi/translation.json";
+import bn from "./locales/bn/translation.json";
+import or from "./locales/or/translation.json";
+import te from "./locales/te/translation.json";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      hi: { translation: hi },
+      bn: { translation: bn },
+      or: { translation: or },
+      te: { translation: te }
+    },
+    lng: "en", // default language
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
